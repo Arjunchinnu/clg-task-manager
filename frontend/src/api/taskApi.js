b9abb29d163ccb299752d4907c4ee68608428874
arjunchinnu604@gmail.com <user.email like : "arjunchinnu604@gmail.com>
2026-02-14 09:24:18 +0530

    fixing main.jsx file

--- frontend/src/api/taskApi.js
+++ frontend/src/api/taskApi.js
@@ -13,7 +13,7 @@ export const fetchTaskByIdApi = async (id) => {
   const res = await axiosClient.get(`/tasks/${id}`);
   return res.data; // success,message, data: task
 };
-export const udapteTaskApi = async (id, payload) => {
+export const updateTaskApi = async (id, payload) => {
   const res = await axiosClient.put(`/tasks/${id}`, payload);
   return res.data; // success,message, data: task
 };
